8f6a077f87194af26ff6ef9e398dd46e43fb4599
Akilan99 <37103105+Akilan99@users.noreply.github.com>
2019-06-06 12:34:49 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,11 @@
-x = "malayalam"
-w = "" 
-for i in x: 
-    w = i + w 
-    if (x==w): 
-        print("YES") 
+a=int(input())
+n=a
+d=0
+while (a>0):
+    b=a%10
+    d=d*10+b
+    a=a//10
+if(n==d):
+    print("yes")
+else:
+    print("no"
